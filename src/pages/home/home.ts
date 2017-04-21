699c9574a506934b65988ce55d5de13fa0243332
-KaiK- <enriqueoriol@gmail.com>
2017-04-21 15:59:31 +0200

    initial commit. ion-img not showing correctly

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Agenda, Contact } from "../../providers/agenda";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public items:Array<Contact>;
+
+  constructor(public navCtrl: NavController, public agenda: Agenda) {
+    this.items = this.agenda.getContacts();
+  }
+
+  checkInitialChange(item, itemIndex, items){
+    if(itemIndex == 0)
+      return item.name[0];
+
+    if(item.name[0] != items[itemIndex-1].name[0])
+      return item.name[0];
+    
+    return null;
+  }
+
+}
---
+++ src/providers/agenda.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import * as faker from 'faker';
+
+/*
+  Generated class for the Agenda provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class Agenda {
+
+  constructor() {}
+
+  getContacts():Contact[]{
+    let contacts = [];
+    for(let i=0; i< 10; i++){
+      let contact:Contact ={
+        name:faker.name.findName(),
+        phone:faker.phone.phoneNumber(),
+        imgUrl: faker.image.avatar()
+      }
+      contacts.push(contact);
+    }
+
+    contacts.sort((a:Contact, b:Contact)=>{
+      if (a.name < b.name)
+        return -1;
+      if(a.name > b.name)
+        return 1;
+      else return 0;
+    });
+    
+
+    return contacts;
+  }
+
+}
+
+export interface Contact extends ContactAbstact{
+    name:string,
+    phone:number,
+    imgUrl:string  
+}
+
+abstract class ContactAbstact{
+  constructor(  
+    name:string,
+    phone:number,
+    imgUrl:string
+  ){}
+}
